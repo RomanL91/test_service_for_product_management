7bd94b4bb1084ed9ad1412bd63941cf5f2d40237
RomanL91 <dev_roman91@mail.ru>
2024-06-05 11:48:16 +0500

    установка и проба работы с библиотеками для редактирования изображений

--- core/settings.py
+++ core/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     "app_products",
     "app_category",
     "app_manage_banners",
+        # для работы с изображениями
+    'easy_thumbnails',
+    'image_cropping',
 ]
 
 MIDDLEWARE = [
