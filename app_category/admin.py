1ddd8b30bf164cca6d353c3d0c6820343f1dda82
RomanL91 <dev_roman91@mail.ru>
2024-06-14 00:59:54 +0500

    рефактор после выноса CustomAdminFileWidget

--- app_category/admin.py
+++ app_category/admin.py
@@ -1,35 +1,15 @@
 from django.db import models
 from django.contrib import admin
 
-from django.utils.html import format_html
-from django.contrib.admin.widgets import AdminFileWidget
-
 from django_mptt_admin.admin import DjangoMpttAdmin
 
-from core.mixins import JsonDocumentForm
+from core.mixins import JsonDocumentForm, CustomAdminFileWidget
 from app_category.models import Category, CategoryImage
-
-
-class CustomAdminFileWidget(AdminFileWidget):
-    def render(self, name, value, attrs=None, renderer=None):
-        result = []
-        if hasattr(value, "url"):
-            result.append(
-                f"""<a href="{value.url}" target="_blank">
-                      <img 
-                        src="{value.url}" alt="{value}" 
-                        width="100" height="100"
-                        style="object-fit: cover;"
-                      />
-                    </a>"""
-            )
-        result.append(super().render(name, value, attrs, renderer))
-        return format_html("".join(result))
 
 
 class CategoryImageInline(admin.StackedInline):
     model = CategoryImage
-    max_num = 10
+    max_num = 1
     extra = 0
     formfield_overrides = {models.ImageField: {"widget": CustomAdminFileWidget}}
 
