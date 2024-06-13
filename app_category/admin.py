7918064657950b5115811811bf1047b91bf929f8
RomanL91 <dev_roman91@mail.ru>
2024-06-13 15:26:16 +0500

    админка сущности категория инлайн добавление изображений(миниатюр)

--- app_category/admin.py
+++ app_category/admin.py
@@ -1,9 +1,37 @@
+from django.db import models
 from django.contrib import admin
+
+from django.utils.html import format_html
+from django.contrib.admin.widgets import AdminFileWidget
 
 from django_mptt_admin.admin import DjangoMpttAdmin
 
 from core.mixins import JsonDocumentForm
-from app_category.models import Category
+from app_category.models import Category, CategoryImage
+
+
+class CustomAdminFileWidget(AdminFileWidget):
+    def render(self, name, value, attrs=None, renderer=None):
+        result = []
+        if hasattr(value, "url"):
+            result.append(
+                f"""<a href="{value.url}" target="_blank">
+                      <img 
+                        src="{value.url}" alt="{value}" 
+                        width="100" height="100"
+                        style="object-fit: cover;"
+                      />
+                    </a>"""
+            )
+        result.append(super().render(name, value, attrs, renderer))
+        return format_html("".join(result))
+
+
+class CategoryImageInline(admin.StackedInline):
+    model = CategoryImage
+    max_num = 10
+    extra = 0
+    formfield_overrides = {models.ImageField: {"widget": CustomAdminFileWidget}}
 
 
 @admin.register(Category)
@@ -14,4 +42,7 @@ class CategoryAdmin(DjangoMpttAdmin):
     ]
     search_fields = [
         "name_category",
+    ]
+    inlines = [
+        CategoryImageInline,
     ]
