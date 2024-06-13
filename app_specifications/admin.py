a2f0a5ec6c0250bfea533fbd01dde547f6dc4d26
RomanL91 <dev_roman91@mail.ru>
2024-06-14 02:28:40 +0500

    представление сушности характеристик в админке

--- app_specifications/admin.py
+++ app_specifications/admin.py
@@ -1,3 +1,25 @@
+from django.db import models
 from django.contrib import admin
 
-# Register your models here.
+from flat_json_widget.widgets import FlatJsonWidget
+
+from core.mixins import JsonDocumentForm
+from app_specifications.models import Specifications
+
+
+class SpecificationsInline(admin.StackedInline):
+    model = Specifications
+    max_num = 100
+    extra = 0
+    formfield_overrides = {models.JSONField: {"widget": FlatJsonWidget}}
+
+
+@admin.register(Specifications)
+class SpecificationAdmin(admin.ModelAdmin):
+    form = JsonDocumentForm
+    list_display = [
+        "name_specification",
+    ]
+    search_fields = [
+        "name_specification",
+    ]
