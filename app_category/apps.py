e48d80a803e70ff04df5f656f7ca8aa631634dac
RomanL91 <dev_roman91@mail.ru>
2024-06-14 12:42:52 +0500

    смена названия приложения

--- app_category/apps.py
+++ app_category/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class AppCategoryConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "app_category"
-    verbose_name = "Категории/Брэнды"
+    verbose_name = "Управление категориями"
