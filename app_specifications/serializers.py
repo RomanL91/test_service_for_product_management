6f965c02e931d4c73dcc1c279fde3f0820579b72
RomanL91 <dev_roman91@mail.ru>
2024-06-18 09:29:29 +0500

    создание классов сериализаторов для сущностей Specifications

---
+++ app_specifications/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from app_specifications.models import Specifications, NameSpecifications, ValueSpecifications
+
+
+class NameSpecificationsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NameSpecifications
+        fields = "__all__"
+
+class ValueSpecificationsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ValueSpecifications
+        fields = "__all__"
+
+class SpecificationsSerializer(serializers.ModelSerializer):
+    name_specification = NameSpecificationsSerializer()
+    value_specification = ValueSpecificationsSerializer()
+    class Meta:
+        model = Specifications
+        fields = "__all__"
