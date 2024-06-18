9cd6a0511d32c9d47f04bde410951c2401a87ba9
RomanL91 <dev_roman91@mail.ru>
2024-06-18 12:29:34 +0500

    изменение маршрутов в листе паттернов для приложения_категорий ввиду введения поля slug

--- app_category/urls.py
+++ app_category/urls.py
@@ -9,29 +9,25 @@ translate_category_list = CategoryViewSet.as_view(
         "get": "lang",
     }
 )
-
+category_detail = CategoryViewSet.as_view(
+    {
+        "get": "retrieve",
+    }
+)
 translate_category_detail = CategoryViewSet.as_view(
     {
         "get": "lang_",
     }
 )
 
-# translation_products_category = CategoryViewSet.as_view(
-#     {
-#         "get": "products",
-#     }
-# )
-
 
 urlpatterns = [
     re_path(r"^api/v1/category/lang/(?P<lang>\w+)/$", translate_category_list),
+    re_path(r"^api/v1/category/(?P<slug_cat>[-\w]+)/$", category_detail),
     re_path(
-        r"^api/v1/category/(?P<pk>\d+)/lang_/(?P<lang>\w+)/$", translate_category_detail
+        r"^api/v1/category/(?P<slug_cat>[-\w]+)/lang_/(?P<lang>\w+)/$",
+        translate_category_detail,
     ),
-    # re_path(
-    #     r"^api/v1/category/(?P<pk>\d+)/products/(?P<lang>\w+)/$",
-    #     translation_products_category,
-    # ),
 ]
 
 urlpatterns_category_suff = format_suffix_patterns(urlpatterns)
