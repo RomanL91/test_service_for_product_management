cb75d03aeaf36e1b4098324c802ba8633feac543
RomanL91 <dev_roman91@mail.ru>
2024-06-19 13:52:36 +0500

    регистрация сущностей в админке + инлайн класс для сущности Продукты

--- app_sales_points/admin.py
+++ app_sales_points/admin.py
@@ -1,3 +1,44 @@
+from django.db import models
 from django.contrib import admin
 
-# Register your models here.
+from flat_json_widget.widgets import FlatJsonWidget
+
+from core.mixins import JsonDocumentForm
+
+from app_sales_points.models import City, Warehouse, Stock
+
+
+class StockInline(admin.StackedInline):
+    model = Stock
+    max_num = 100
+    extra = 0
+    autocomplete_fields = ["warehouse"]
+    formfield_overrides = {models.JSONField: {"widget": FlatJsonWidget}}
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    form = JsonDocumentForm
+
+
+@admin.register(Warehouse)
+class WarehouseAdmin(admin.ModelAdmin):
+    form = JsonDocumentForm
+    search_fields = [
+        "name_warehouse",
+    ]
+
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    form = JsonDocumentForm
+    list_display = [
+        "product",
+    ]
+    # search_fields = [
+    #     "name_specification",
+    # ]
+    autocomplete_fields = [
+        "product",
+        "warehouse",
+    ]
