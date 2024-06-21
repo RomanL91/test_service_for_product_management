5fd453112986a283af05c5f977b8a579436914c7
RomanL91 <dev_roman91@mail.ru>
2024-06-21 23:35:50 +0500

    рефактор представления сущности StocksViewSet

--- app_sales_points/views.py
+++ app_sales_points/views.py
@@ -1,46 +1,34 @@
-from django.shortcuts import get_object_or_404
-
 from rest_framework import viewsets
 from rest_framework.response import Response
 
 from app_sales_points.models import Stock
-from app_products.models import Products
 from app_sales_points.serializers import StockSerializer
+
+from core.lang_utils import TranslateManager
 
 
 class StocksViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
 
-    def lang(self, request, lang=None, *args, **kwargs):
-        stocks_queryset = self.get_queryset()
-        serializer = self.get_serializer(stocks_queryset, many=True)
-        data_translate = self.translate_data(serializer.data, lang)
-        return Response(data_translate)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.translate_manager = TranslateManager(self)
 
-    def filter_by_prod(self, request, slug_prod=None, *args, **kwargs):
-        product = get_object_or_404(Products, slug=slug_prod)
-        stocks = Stock.objects.filter(product=product)
+    def filter_by_prod(
+        self, request, prod_pk, city_pk=None, lang=None, *args, **kwargs
+    ):
+        stocks = Stock.objects.filter(product_id=prod_pk, warehouse__city=city_pk)
+        if lang is not None:
+            self.translate_data(stocks, lang)
         serializer = self.get_serializer(stocks, many=True)
         return Response(serializer.data)
 
     def translate_data(self, data, lang):
-        if lang is not None:
-            lang = lang.upper()
-            for el in data:
-                el["warehouse"] = self.fields_translate(
-                    el["warehouse"], "name_warehouse", lang
-                )
-                el["warehouse"]["city"] = self.fields_translate(
-                    el["warehouse"]["city"], "name_city", lang
-                )
-        return data
-
-    def fields_translate(self, field, name_field, lang):
-        traslate_data = field.get("additional_data", {})
-        traslate_value = traslate_data.get(lang, field[name_field])
-
-        if traslate_value != "":
-            field[name_field] = traslate_value
-
-        return field
+        for el in data:
+            el.warehouse = self.translate_manager.translate_instance(
+                el.warehouse, "name_warehouse", lang
+            )
+            el.warehouse.city = self.translate_manager.translate_instance(
+                el.warehouse.city, "name_city", lang
+            )
