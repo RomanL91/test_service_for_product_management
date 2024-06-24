cf37e996b5d3c7d977025bfb21bc77a9fa29bb81
RomanL91 <dev_roman91@mail.ru>
2024-06-24 17:56:34 +0500

    представления для сущности Specifications

--- app_specifications/views.py
+++ app_specifications/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from app_specifications.models import Specifications
+from app_specifications.serializers import SpecificationsSerializer
+
+from core.lang_utils import TranslateManager
+
+
+class SpecificationsViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Specifications.objects.all()
+    serializer_class = SpecificationsSerializer
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.translate_manager = TranslateManager(self)
+
+    def filter_by_prod(self, request, prod_pk, lang=None, *args, **kwargs):
+        specif = Specifications.objects.filter(product_id=prod_pk)
+        serializer = self.get_serializer(specif, many=True)
+        if lang is not None:
+            self.translate_manager.translate_nested_fields(
+                serializer.data, ["name_specification", "value_specification"], lang
+            )
+        return Response(serializer.data)
