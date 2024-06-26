cd74c4eebe323c5e1e518908bedc612d02893fdc
RomanL91 <dev_roman91@mail.ru>
2024-06-26 16:38:51 +0500

    модель для управления банерами

--- app_manage_banners/models.py
+++ app_manage_banners/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+from app_category.models import Category
+
+
+class BannerImage(models.Model):
+    name_banner = models.CharField(
+        max_length=150,
+        verbose_name="Название банера",
+    )
+    image = models.ImageField(
+        verbose_name="Изображение банера",
+        blank=True,
+        upload_to="product_images/%Y/%m/%d/%H/%M/%S/",
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        verbose_name="Категория",
+    )
+
+    class Meta:
+        verbose_name = "Банер"
+        verbose_name_plural = "Баннеры"
+
+    def __str__(self) -> str:
+        return self.name_banner
+
+    def clean(self):
+        if self.category.level != 0:
+            raise ValidationError(
+                {
+                    "category": "Баннер может быть создан только для категорий с уровнем 0."
+                }
+            )
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
