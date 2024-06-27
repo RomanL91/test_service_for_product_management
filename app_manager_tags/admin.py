e4fc0749ab4126507cc68468f0ac4b07ea333f19
RomanL91 <dev_roman91@mail.ru>
2024-06-27 09:32:23 +0500

    регистрация сущности ТЕГ в админке + небольная настройка отображения полей

--- app_manager_tags/admin.py
+++ app_manager_tags/admin.py
@@ -1,3 +1,14 @@
+from django import forms
 from django.contrib import admin
 
-# Register your models here.
+from core.mixins import JsonDocumentForm
+
+from app_manager_tags.models import Tag, ColorField
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    form = JsonDocumentForm
+    formfield_overrides = {
+        ColorField: {"widget": forms.TextInput(attrs={"type": "color"})}
+    }
