a896cfe3a2b13de223761cfe2557c0455160b4e4
RomanL91 <dev_roman91@mail.ru>
2024-06-28 16:16:46 +0500

    регистрация машрутов представления сущности блог

--- core/router.py
+++ core/router.py
@@ -4,10 +4,12 @@ from app_products.views import ProductsViewSet, PopulatesProductsViewSet
 from app_category.views import CategoryViewSet
 from app_sales_points.views import StocksViewSet
 from app_specifications.views import SpecificationsViewSet
+from app_blogs.views import BlogViewSet
 
 
 router = routers.DefaultRouter()
 
+router.register(r"blog", BlogViewSet)
 router.register(r"populates", PopulatesProductsViewSet)
 router.register(r"products", ProductsViewSet)
 router.register(r"category", CategoryViewSet)
