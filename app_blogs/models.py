d569340f679081604c2037fd1391904a5ba6462d
RomanL91 <dev_roman91@mail.ru>
2024-06-28 16:13:19 +0500

    описание сущности блок для приложения

--- app_blogs/models.py
+++ app_blogs/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+from core.mixins import JSONFieldsMixin, JSONFieldsDescMixin
+
+from app_products.models import Products
+
+
+class Blog(JSONFieldsMixin, JSONFieldsDescMixin, models.Model):
+    title_blog = models.CharField(
+        max_length=150,
+        verbose_name="Заголовок блога",
+    )
+    body_blog = models.TextField(
+        verbose_name="Текст блога",
+    )
+    related_product = models.ManyToManyField(
+        Products,
+        blank=True,
+        verbose_name="Сопутствующий товар",
+    )
+
+    class Meta:
+        verbose_name = "Статья"
+        verbose_name_plural = "Статьи"
+
+    def __str__(self) -> str:
+        return self.title_blog
+
+
+class BlogImage(models.Model):
+    image = models.ImageField(
+        verbose_name="Изображение",
+        blank=True,
+        upload_to="product_images/%Y/%m/%d/%H/%M/%S/",
+    )
+    blog = models.ForeignKey(
+        Blog,
+        on_delete=models.CASCADE,
+        verbose_name="Продукт",
+    )
+
+    class Meta:
+        verbose_name = "Изображение"
+        verbose_name_plural = "Изображения"
+
+    def __str__(self) -> str:
+        return self.blog.title_blog
