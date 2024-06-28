508423290732b74f813cca781ec271fbf0bbe12f
RomanL91 <dev_roman91@mail.ru>
2024-06-28 16:14:07 +0500

    регистрация сущности блог в админке

--- app_blogs/admin.py
+++ app_blogs/admin.py
@@ -1,3 +1,49 @@
+from django.db import models
 from django.contrib import admin
 
-# Register your models here.
+from django.utils.html import mark_safe
+
+from core.mixins import JsonDocumentForm, CustomAdminFileWidget
+
+from app_blogs.models import Blog, BlogImage
+
+
+class BlogImageInline(admin.StackedInline):
+    model = BlogImage
+    max_num = 3
+    extra = 0
+    formfield_overrides = {models.ImageField: {"widget": CustomAdminFileWidget}}
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    form = JsonDocumentForm
+    filter_horizontal = [
+        "related_product",
+    ]
+    inlines = [
+        BlogImageInline,
+    ]
+
+
+@admin.register(BlogImage)
+class BlogtImageAdmin(admin.ModelAdmin):
+    readonly_fields = ["preview"]
+    list_display = [
+        "get_image",
+        "blog",
+    ]
+
+    def preview(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="600" ')
+
+    preview.short_description = "Предпоказ"
+
+    def get_image(self, obj):
+        try:
+            url_prod = obj.image.url
+            return mark_safe(f'<img src={url_prod} width="75"')
+        except:
+            return None
+
+    get_image.short_description = "ФОТО"
