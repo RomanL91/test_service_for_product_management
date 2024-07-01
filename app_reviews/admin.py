a7965a5f735a3256fcc114dc37a575f9a0e110ab
RomanL91 <dev_roman91@mail.ru>
2024-07-01 21:40:57 +0500

    регистрация сущности в админ панели + имя собственное для приложения

--- app_reviews/admin.py
+++ app_reviews/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from app_reviews.models import Review
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = [
+        "product",
+        "moderation",
+    ]
+    list_filter = [
+        "moderation",
+    ]
