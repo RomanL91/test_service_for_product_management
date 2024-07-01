8a5a54e57adc046632607d304de4bec7a5482003
RomanL91 <dev_roman91@mail.ru>
2024-07-01 21:39:42 +0500

    первичное представление и настройка сущности отзывы

--- app_reviews/models.py
+++ app_reviews/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from app_products.models import Products
+
+
+def valid(value):
+    if not 5 >= value >= 0:
+        raise ValidationError(
+            _("%(value)s Оценка может быть от 0 до 5"),
+            params={
+                "value": value,
+            },
+        )
+
+
+class Review(models.Model):
+
+    rating = models.PositiveIntegerField(
+        verbose_name="Оценка/Рейтинг",
+        default=5,
+        validators=[
+            valid,
+        ],
+    )
+    review = models.TextField(
+        verbose_name="Отзыв",
+        max_length=2000,
+        unique=True,
+    )
+    moderation = models.BooleanField(
+        verbose_name="Модерация",
+        default=False,
+    )
+    product = models.ForeignKey(
+        Products,
+        on_delete=models.CASCADE,
+        verbose_name="Продукт",
+    )
+    user_id = models.IntegerField(
+        verbose_name="ID пользователя",
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "Отзыв/Рейтинг"
+        verbose_name_plural = "Отзывы/Рейтинги"
+
+    def __str__(self) -> str:
+        return self.product.name_product
