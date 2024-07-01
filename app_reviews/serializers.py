bc6ad3e0fee9370b9d0faed969d049d9c75c7105
RomanL91 <dev_roman91@mail.ru>
2024-07-01 21:42:47 +0500

    сериализатор для сущности отзывы

---
+++ app_reviews/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from app_reviews.models import Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    jwt_token = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = Review
+        fields = "__all__"
+        read_only_fields = ("moderation", "user_id")
+
+    def create(self, validated_data):
+        jwt_token = validated_data.pop(
+            "jwt_token", None
+        )  # Извлекаем jwt_token из validated_data
+        instance = super().create(validated_data)  # Создаем объект Review
+
+        # Дополнительная логика с jwt_token, если необходимо
+
+        return instance
