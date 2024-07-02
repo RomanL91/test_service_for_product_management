b4a03b7543bae42fb29fa4c4e14c376d918f7409
RomanL91 <dev_roman91@mail.ru>
2024-07-02 13:44:43 +0500

    регистрация маршрута для фильтрации описаний по ид продукта

---
+++ app_descriptions/urls.py
@@ -0,0 +1,20 @@
+from django.urls import re_path
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from app_descriptions.views import ProductDescriptionViewSet
+
+
+filter_by_prod = ProductDescriptionViewSet.as_view(
+    {
+        "get": "filter_by_prod",
+    }
+)
+
+urlpatterns = [
+    re_path(
+        r"^api/v1/descrip/filter_by_prod/(?P<prod_pk>\d+)/$",
+        filter_by_prod,
+    ),
+]
+
+urlpatterns_descrip_suff = format_suffix_patterns(urlpatterns)
