0e23544023706bea61af94bd3bb3c6bcca3a1703
RomanL91 <dev_roman91@mail.ru>
2024-07-02 13:42:03 +0500

    представление для сущности описаний

--- app_descriptions/views.py
+++ app_descriptions/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from app_descriptions.models import ProductDescription
+from app_descriptions.serializers import ProductDescriptionSerializer
+
+from core.lang_utils import TranslateManager
+
+
+class ProductDescriptionViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = ProductDescription.objects.all()
+    serializer_class = ProductDescriptionSerializer
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.translate_manager = TranslateManager(self)
+
+    def filter_by_prod(self, request, prod_pk, *args, **kwargs):
+        specif = ProductDescription.objects.filter(product_id=prod_pk)
+        serializer = self.get_serializer(specif, many=True)
+        return Response(serializer.data)
