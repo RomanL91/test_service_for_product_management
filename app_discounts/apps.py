511c13998ce609879596a5a74a4672f9d3706cbf
RomanL91 <dev_roman91@mail.ru>
2024-07-08 23:51:30 +0500

    имя собственное для приложения

--- app_discounts/apps.py
+++ app_discounts/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AppDiscountsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "app_discounts"
+    verbose_name = "Управление скидками"
