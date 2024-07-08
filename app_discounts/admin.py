fb495bf02a254bf5938b38f822837af5ae597e23
RomanL91 <dev_roman91@mail.ru>
2024-07-08 16:57:33 +0500

    регистрация сущностей в админ панели

--- app_discounts/admin.py
+++ app_discounts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.mixins import JsonDocumentForm
+from app_discounts.models import ProductDiscount, CategoryDiscount
+
+
+class BaseDiscountAdmin(admin.ModelAdmin):
+    form = JsonDocumentForm
+    list_display = [
+        "name",
+        "is_currently_active",
+    ]
+
+    def is_currently_active(self, obj):
+        return obj.is_valid()
+
+    is_currently_active.boolean = True
+    is_currently_active.short_description = "Активна сейчас?"
+
+
+@admin.register(ProductDiscount)
+class ProductDiscountAdmin(BaseDiscountAdmin):
+    filter_horizontal = ["products"]
+
+
+@admin.register(CategoryDiscount)
+class CategoryDiscountAdmin(BaseDiscountAdmin):
+    filter_horizontal = ["categories"]
