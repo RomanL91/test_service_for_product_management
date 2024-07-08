2f28eb5bada2218000d2db1d3e1310aa973d8154
RomanL91 <dev_roman91@mail.ru>
2024-07-09 01:32:41 +0500

    описание сущности услуга

--- app_services/models.py
+++ app_services/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from core.mixins import JSONFieldsMixin
+
+from app_sales_points.models import City
+
+
+class Service(JSONFieldsMixin, models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    cities = models.ManyToManyField(City, related_name="services")
+
+    class Meta:
+        verbose_name = "Сервис"
+        verbose_name_plural = "Сервисы"
+
+    def __str__(self):
+        return self.name
