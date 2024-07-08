89910800c6a967f7f310c91c74aebd39958dca48
RomanL91 <dev_roman91@mail.ru>
2024-07-09 01:34:23 +0500

    регистрация сущности услуга в админке

--- app_services/admin.py
+++ app_services/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.mixins import JsonDocumentForm
+
+from app_services.models import Service
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    form = JsonDocumentForm
