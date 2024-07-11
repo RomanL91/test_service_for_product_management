f9aed34dd4fb21fa38909e7e6dcd6e1bd6940a1e
RomanL91 <dev_roman91@mail.ru>
2024-07-11 17:02:32 +0500

    эндпоинт для получения товаров уже с аннотацией по списку ИД

--- app_products/urls.py
+++ app_products/urls.py
@@ -27,23 +27,38 @@ slugs = ProductsViewSet.as_view(
         "get": "slugs",
     }
 )
+products_by_ids = ProductsViewSet.as_view({"get": "get_products_by_ids"})
 
 urlpatterns = [
-    re_path(r"^api/v1/products/lang/(?P<lang>\w+)/$", products_list),
+    re_path(
+        r"^api/v1/products/lang/(?P<lang>\w+)/$",
+        products_list,
+    ),
     re_path(
         r"^api/v1/products/(?P<slug_prod>[-\w]+)/lang/(?P<lang>\w+)/$",
         products_detail,
     ),
-    re_path(r"^api/v1/products/filter_by_cat/(?P<slug_cat>[-\w]+)/$", filter_by_cat),
+    re_path(
+        r"^api/v1/products/filter_by_cat/(?P<slug_cat>[-\w]+)/$",
+        filter_by_cat,
+    ),
     re_path(
         r"^api/v1/products/filter_by_cat/(?P<slug_cat>[-\w]+)/lang/(?P<lang>\w+)/$",
         filter_by_cat,
     ),
     re_path(r"^api/v1/products/all/slugs/$", slugs),
-    re_path(r"^api/v1/products/(?P<slug_prod>[-\w]+)/(?P<ids>[\d,]+)/$", products_list),
+    re_path(
+        r"^api/v1/products/(?P<slug_prod>[-\w]+)/(?P<ids>[\d,]+)/$",
+        products_list,
+    ),
     re_path(
         r"^api/v1/products/(?P<slug_prod>[-\w]+)/(?P<ids>[\d,]+)/lang/(?P<lang>\w+)/$",
         products_list,
+    ),
+    re_path(
+        r"^api/v1/products/by_ids/(?P<ids>[\d,]+)/$",
+        products_by_ids,
+        name="products-by-ids",
     ),
 ]
 
