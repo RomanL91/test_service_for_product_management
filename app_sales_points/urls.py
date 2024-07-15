a0b8021234b32ee9232f2b5e0b980f26ead97d8d
RomanL91 <dev_roman91@mail.ru>
2024-07-16 01:46:57 +0500

    удаление логики для переводов полей

--- app_sales_points/urls.py
+++ app_sales_points/urls.py
@@ -15,10 +15,6 @@ urlpatterns = [
         r"^api/v1/stocks/filter_by_prod/(?P<prod_pk>\d+)/(?P<city_pk>\d+)/$",
         filter_by_prod,
     ),
-    re_path(
-        r"^api/v1/stocks/filter_by_prod/(?P<prod_pk>\d+)/(?P<city_pk>\d+)/lang/(?P<lang>\w+)/$",
-        filter_by_prod,
-    ),
 ]
 
 urlpatterns_stocks_suff = format_suffix_patterns(urlpatterns)
