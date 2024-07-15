ae2b7496b12fe19a6ea17f8c10a5da6bacb530db
RomanL91 <dev_roman91@mail.ru>
2024-07-15 22:49:37 +0500

    удаление не испоользуемого

--- app_specifications/views.py
+++ app_specifications/views.py
@@ -4,22 +4,12 @@ from rest_framework.response import Response
 from app_specifications.models import Specifications
 from app_specifications.serializers import SpecificationsSerializer
 
-from core.lang_utils import TranslateManager
-
 
 class SpecificationsViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Specifications.objects.all()
     serializer_class = SpecificationsSerializer
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.translate_manager = TranslateManager(self)
-
-    def filter_by_prod(self, request, prod_pk, lang=None, *args, **kwargs):
+    def filter_by_prod(self, request, prod_pk, *args, **kwargs):
         specif = Specifications.objects.filter(product_id=prod_pk)
         serializer = self.get_serializer(specif, many=True)
-        if lang is not None:
-            self.translate_manager.translate_nested_fields(
-                serializer.data, ["name_specification", "value_specification"], lang
-            )
         return Response(serializer.data)
