98281758488cf98071f0d29543b378413e76db9a
RomanL91 <dev_roman91@mail.ru>
2024-07-16 03:29:25 +0500

    создание маршрута

--- app_sales_points/urls.py
+++ app_sales_points/urls.py
@@ -9,11 +9,20 @@ filter_by_prod = StocksViewSet.as_view(
         "get": "filter_by_prod",
     }
 )
+get_prices_by_category = StocksViewSet.as_view(
+    {
+        "get": "get_prices_by_category",
+    }
+)
 
 urlpatterns = [
     re_path(
         r"^api/v1/stocks/filter_by_prod/(?P<prod_pk>\d+)/(?P<city_pk>\d+)/$",
         filter_by_prod,
+    ),
+    re_path(
+        r"^api/v1/stocks/prices_by_category/(?P<cat_pk>\d+)/$",
+        get_prices_by_category,
     ),
 ]
 
