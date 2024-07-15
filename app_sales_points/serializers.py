8fc27703aa68b455fe75d58da114e74729610d5e
RomanL91 <dev_roman91@mail.ru>
2024-07-16 03:28:43 +0500

    сериализатор для нового метода

--- app_sales_points/serializers.py
+++ app_sales_points/serializers.py
@@ -23,3 +23,22 @@ class StockSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stock
         fields = "__all__"
+
+
+class PriceRangeByCitySerializer(serializers.Serializer):
+    city = serializers.CharField(
+        source="warehouse__city__name_city",
+        read_only=True,
+    )
+    min_price = serializers.DecimalField(
+        source="MinPrice",
+        max_digits=10,
+        decimal_places=2,
+        read_only=True,
+    )
+    max_price = serializers.DecimalField(
+        source="MaxPrice",
+        max_digits=10,
+        decimal_places=2,
+        read_only=True,
+    )
