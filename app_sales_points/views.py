85e235395f79b60b7892171551aeec1fafd7264d
RomanL91 <dev_roman91@mail.ru>
2024-07-16 03:27:58 +0500

    метод view StocksViewSet для получения минимальной и максимальной цены по городам от категории и ее подкатегорий

--- app_sales_points/views.py
+++ app_sales_points/views.py
@@ -1,15 +1,47 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 
+from django.db.models import Min, Max
+
 from app_sales_points.models import Stock
-from app_sales_points.serializers import StockSerializer
+from app_category.models import Category
+from app_products.models import Products
+from app_sales_points.serializers import StockSerializer, PriceRangeByCitySerializer
 
 
 class StocksViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Stock.objects.all()
+    queryset = Stock.objects.select_related(
+        "warehouse__city", "product__category"
+    ).all()
     serializer_class = StockSerializer
 
     def filter_by_prod(self, request, prod_pk, city_pk=None, *args, **kwargs):
         stocks = Stock.objects.filter(product_id=prod_pk, warehouse__city=city_pk)
         serializer = self.get_serializer(stocks, many=True)
+        return Response(serializer.data)
+
+    def get_prices_by_category(self, request, cat_pk):
+        # Получаем категорию и все её подкатегории
+        descendant_categories = (
+            Category.objects.get(pk=cat_pk)
+            .get_descendants(include_self=True)
+            .values_list("id", flat=True)
+        )
+
+        # Получаем продукты из этих категорий
+        product_ids = Products.objects.filter(
+            category_id__in=descendant_categories
+        ).values_list("id", flat=True)
+
+        # Получаем цены на эти продукты, группируя по городам складов
+        prices = (
+            Stock.objects.filter(product_id__in=product_ids)
+            .select_related("warehouse__city")
+            .values("warehouse__city__name_city")
+            .annotate(MinPrice=Min("price"), MaxPrice=Max("price"))
+            .order_by("warehouse__city__name_city")
+        )
+
+        # Сериализуем данные
+        serializer = PriceRangeByCitySerializer(prices, many=True)
         return Response(serializer.data)
