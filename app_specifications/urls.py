54f57a8201781c5147bc7ca4e0277f1912b5b932
RomanL91 <dev_roman91@mail.ru>
2024-07-16 00:54:39 +0500

    ендпоинт для получения характеристик продуктов от выбранной категории

--- app_specifications/urls.py
+++ app_specifications/urls.py
@@ -9,8 +9,18 @@ filter_by_prod = SpecificationsViewSet.as_view(
         "get": "filter_by_prod",
     }
 )
+filter_by_cat = SpecificationsViewSet.as_view(
+    {
+        "get": "filter_by_cat",
+    }
+)
 
 urlpatterns = [
+    re_path(
+        r"api/v1/specif/by_category/(?P<cat_pk>\d+)/$",
+        filter_by_cat,
+        name="specifications-by-category",
+    ),
     re_path(
         r"^api/v1/specif/filter_by_prod/(?P<prod_pk>\d+)/$",
         filter_by_prod,
--- app_specifications/views.py
+++ app_specifications/views.py
@@ -2,14 +2,34 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 
 from app_specifications.models import Specifications
+from app_category.models import Category
 from app_specifications.serializers import SpecificationsSerializer
 
 
 class SpecificationsViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Specifications.objects.all()
+    queryset = Specifications.objects.select_related(
+        "name_specification", "value_specification"
+    )
     serializer_class = SpecificationsSerializer
 
     def filter_by_prod(self, request, prod_pk, *args, **kwargs):
         specif = Specifications.objects.filter(product_id=prod_pk)
         serializer = self.get_serializer(specif, many=True)
+        return Response(serializer.data)
+
+    def filter_by_cat(self, request, cat_pk):
+        # Находим категорию и все её подкатегории
+        descendant_categories = (
+            Category.objects.get(pk=cat_pk)
+            .get_descendants(include_self=True)
+            .values_list("id", flat=True)
+        )
+
+        # Получаем характеристики для продуктов, которые принадлежат этим категориям
+        specifications = Specifications.objects.filter(
+            product__category__id__in=descendant_categories
+        ).select_related("name_specification", "value_specification")
+
+        # Сериализуем данные
+        serializer = self.get_serializer(specifications, many=True)
         return Response(serializer.data)
