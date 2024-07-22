70d71c65e860653b7dc0895b435564706516e71f
RomanL91 <dev_roman91@mail.ru>
2024-07-22 14:09:15 +0500

    маршрутизация для метода получения характеристик продуктов - комплектации

--- app_specifications/urls.py
+++ app_specifications/urls.py
@@ -14,6 +14,11 @@ filter_by_cat = SpecificationsViewSet.as_view(
         "get": "filter_by_cat",
     }
 )
+get_specif_configurations = SpecificationsViewSet.as_view(
+    {
+        "get": "get_specif_product_configurations",
+    }
+)
 
 urlpatterns = [
     re_path(
@@ -24,6 +29,11 @@ urlpatterns = [
     re_path(
         r"^api/v1/specif/filter_by_prod/(?P<prod_pk>\d+)/$",
         filter_by_prod,
+    ),
+    re_path(
+        r"^api/v1/specif/configurations/(?P<ids>[\d,]+)/$",
+        get_specif_configurations,
+        name="specifications-configurations",
     ),
 ]
 
