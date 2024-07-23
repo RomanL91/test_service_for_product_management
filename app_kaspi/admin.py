494b2ec2f9aac07c6419afd606d3765258d785a9
RomanL91 <dev_roman91@mail.ru>
2024-07-23 15:27:56 +0500

    регистрация моделей в админке

--- app_kaspi/admin.py
+++ app_kaspi/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
-from app_kaspi.models import Token
+from app_kaspi.models import Token, Order, Customer, Address, Product, KaspiDelivery
 
 
 @admin.register(Token)
 class TokenAdmin(admin.ModelAdmin):
     pass
+
+
+admin.site.register(Order)
+admin.site.register(Customer)
+admin.site.register(Address)
+admin.site.register(Product)
+admin.site.register(KaspiDelivery)
