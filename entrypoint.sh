ebe8e6934593b12a0ece3a57c2efa3af728e4640
RomanL91 <dev_roman91@mail.ru>
2024-07-24 01:40:58 +0500

    точка входа, выполнение команд

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# python manage.py makemigrations
+# python manage.py migrate
+python manage.py collectstatic --no-input
+
+python -m celery -A core.celery worker -l info -c 2 -P eventlet &
+python -m celery -A core beat -l info &
+python -m celery -A core flower &
+
+
+# gunicorn --bind 0.0.0.0:8000 core.asgi -w 4 -k uvicorn.workers.UvicornWorker
+uvicorn core.asgi:application --host 0.0.0.0 --port 8000
