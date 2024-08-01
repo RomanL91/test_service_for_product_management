e4b061f468f061e3c42f45af542d144443f931fb
RomanL91 <dev_roman91@mail.ru>
2024-08-01 11:31:04 +0500

    добавление endpoint для получения брендов по слагу категории

--- app_brands/urls.py
+++ app_brands/urls.py
@@ -4,17 +4,27 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from app_brands.views import BrandsViewSet
 
 
-filter_by_cat = BrandsViewSet.as_view(
+filter_by_cat_id = BrandsViewSet.as_view(
     {
-        "get": "filter_by_cat",
+        "get": "filter_by_cat_id",
+    }
+)
+filter_by_cat_slug = BrandsViewSet.as_view(
+    {
+        "get": "filter_by_cat_slug",
     }
 )
 
 urlpatterns = [
     re_path(
-        r"api/v1/brands/by_category/(?P<cat_pk>\d+)/$",
-        filter_by_cat,
-        name="brands-by-category",
+        r"api/v1/brands/by_category/id/(?P<cat_pk>\d+)/$",
+        filter_by_cat_id,
+        name="brands-by-category_id",
+    ),
+    re_path(
+        r"api/v1/brands/by_category/slug/(?P<cat_slug>\w+)/$",
+        filter_by_cat_slug,
+        name="brands-by-category_slug",
     ),
 ]
 
--- app_brands/views.py
+++ app_brands/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 
+from django.shortcuts import get_object_or_404
+
 from app_brands.models import Brands
 from app_category.models import Category
 from app_brands.serializers import BrandsSerializer
@@ -10,19 +12,25 @@ class BrandsViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Brands.objects.all()
     serializer_class = BrandsSerializer
 
-    def filter_by_cat(self, request, cat_pk):
-        # Находим категорию и все её подкатегории
-        descendant_categories = (
-            Category.objects.get(pk=cat_pk)
-            .get_descendants(include_self=True)
-            .values_list("id", flat=True)
+    def filter_brands_by_category(self, **category_filter):
+        # Получаем категорию по заданному фильтру (id или slug)
+        category = get_object_or_404(Category, **category_filter)
+        # Находим все подкатегории
+        descendant_categories = category.get_descendants(include_self=True).values_list(
+            "id", flat=True
         )
-        # Получаем продукты, которые принадлежат этим категориям
+        # Фильтруем бренды по этим категориям
         brands = (
             Brands.objects.filter(products__category__id__in=descendant_categories)
             .distinct()
             .order_by("name_brand")
         )
-
+        # Сериализуем и возвращаем данные
         serializer = self.get_serializer(brands, many=True)
         return Response(serializer.data)
+
+    def filter_by_cat_id(self, request, cat_pk):
+        return self.filter_brands_by_category(id=cat_pk)
+
+    def filter_by_cat_slug(self, request, cat_slug):
+        return self.filter_brands_by_category(slug=cat_slug)
