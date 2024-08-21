8258bbd357b344d5c0782130af6d1ea35880858a
RomanL91 <dev_roman91@mail.ru>
2024-08-21 14:54:48 +0500

    маршрутизация для ордеров

--- app_orders/urls.py
+++ app_orders/urls.py
@@ -3,5 +3,6 @@ from .views import OrdersListView, OrderDetailView
 
 urlpatterns = [
     path("orders/", OrdersListView.as_view(), name="orders_list"),
+    path("orders/<int:pk>/", OrdersListView.as_view(), name="archive_orders_list"),
     path("order/<uuid:uuid_id>/", OrderDetailView.as_view(), name="order-detail"),
 ]
