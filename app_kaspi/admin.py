42efea195d991ef805088c375eb954bf50d33544
RomanL91 <dev_roman91@mail.ru>
2024-10-28 08:33:16 +0500

    спратал app_kaspi, снял регистрацию в админке

--- app_kaspi/admin.py
+++ app_kaspi/admin.py
@@ -1,22 +1,22 @@
-from django.contrib import admin
+# from django.contrib import admin
 
-from app_kaspi.models import Token, Order, Customer, Product
-
-
-@admin.register(Token)
-class TokenAdmin(admin.ModelAdmin):
-    pass
+# from app_kaspi.models import Token, Order, Customer, Product
 
 
-@admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
-    list_display = [
-        "state",
-        "status",
-        "customer_kaspi",
-        "product_in_orders",
-    ]
+# @admin.register(Token)
+# class TokenAdmin(admin.ModelAdmin):
+#     pass
 
 
-admin.site.register(Customer)
-admin.site.register(Product)
+# @admin.register(Order)
+# class OrderAdmin(admin.ModelAdmin):
+#     list_display = [
+#         "state",
+#         "status",
+#         "customer_kaspi",
+#         "product_in_orders",
+#     ]
+
+
+# admin.site.register(Customer)
+# admin.site.register(Product)
