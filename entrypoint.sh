431dc5ca2d5bab885e4c9f57a0efa87832f3a9da
RomanL91 <dev_roman91@mail.ru>
2024-11-20 14:57:01 +0500

    небольшая прака - перенос строки

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,12 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input
-python manage.py search_index --rebuild -f
 
 python -m celery -A core.celery worker -l info -c 2 -P eventlet &
 python -m celery -A core beat -l info &
 python -m celery -A core flower --port=8001 --basic_auth=admin:admin &
 
+python manage.py search_index --rebuild -f
 
 # gunicorn --bind 0.0.0.0:8000 core.asgi -w 4 -k uvicorn.workers.UvicornWorker
 uvicorn core.asgi:application --host 0.0.0.0 --port 8000
