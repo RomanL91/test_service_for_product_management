0c10066355c8c190ed0dfb43169f2cd03d85bc09
RomanL91 <dev_roman91@mail.ru>
2025-01-02 18:48:09 +0500

    сериализатор для ребер

--- app_sales_points/serializers.py
+++ app_sales_points/serializers.py
@@ -1,6 +1,8 @@
+from datetime import date, timedelta
+
 from rest_framework import serializers
 
-from app_sales_points.models import City, Warehouse, Stock
+from app_sales_points.models import City, Warehouse, Stock, Edges
 
 
 class CitySerializer(serializers.ModelSerializer):
@@ -48,3 +50,32 @@ class StockSerializerElasticSearch(serializers.Serializer):
     city = serializers.CharField()
     quantity = serializers.IntegerField()
     price = serializers.DecimalField(max_digits=10, decimal_places=2)
+
+
+# Сериализатор для Edges
+class EdgeSerializer(serializers.ModelSerializer):
+    city_from = serializers.StringRelatedField()
+    city_to = serializers.StringRelatedField()
+    estimated_delivery_days = serializers.IntegerField(read_only=True)
+    estimated_delivery_date = serializers.SerializerMethodField()
+    transportation_cost = serializers.FloatField()
+
+    class Meta:
+        model = Edges
+        fields = [
+            "city_from",
+            "city_to",
+            "estimated_delivery_days",
+            "estimated_delivery_date",
+            "transportation_cost",
+            "is_active",
+            "expiration_date",
+        ]
+
+    def get_estimated_delivery_date(self, obj):
+        if obj.estimated_delivery_days:
+            self.estimated_delivery_date = date.today() + timedelta(
+                days=obj.estimated_delivery_days
+            )
+            return self.estimated_delivery_date
+        return 0
