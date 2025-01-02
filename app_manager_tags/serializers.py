d2a22b36b8f123887c4f5b21dc9594adcd68d58a
RomanL91 <dev_roman91@mail.ru>
2025-01-02 18:17:01 +0500

    явное указание полей для сериализации для сушности Тег

--- app_manager_tags/serializers.py
+++ app_manager_tags/serializers.py
@@ -6,4 +6,10 @@ from app_manager_tags.models import Tag
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
-        fields = "__all__"
+        fields = [
+            "id",
+            "tag_text",
+            "font_color",
+            "fill_color",
+            "additional_data",
+        ]
