636ae8092622ee7e60e12f47b700a8a8458142bc
RomanL91 <dev_roman91@mail.ru>
2025-01-02 17:24:45 +0500

    возвращены методы для посика описаний по id товара

--- app_descriptions/urls.py
+++ app_descriptions/urls.py
@@ -1,22 +1,21 @@
-# НЕ ИСПОЛЬЗУЕМ
+from django.urls import re_path
+from rest_framework.urlpatterns import format_suffix_patterns
 
-# from django.urls import re_path
-# from rest_framework.urlpatterns import format_suffix_patterns
-
-# from app_descriptions.views import ProductDescriptionViewSet
+from app_descriptions.views import ProductDescriptionViewSet
 
 
-# filter_by_prod = ProductDescriptionViewSet.as_view(
-#     {
-#         "get": "filter_by_prod",
-#     }
-# )
+filter_by_prod = ProductDescriptionViewSet.as_view(
+    {
+        "get": "filter_by_prod",
+    }
+)
 
-# urlpatterns = [
-#     re_path(
-#         r"^api/v1/descrip/filter_by_prod/(?P<prod_pk>\d+)/$",
-#         filter_by_prod,
-#     ),
-# ]
+urlpatterns = [
+    re_path(
+        r"^api/v1/descrip/filter_by_prod/(?P<prod_pk>\d+)/$",
+        filter_by_prod,
+        name="all_desc_to_product",
+    ),
+]
 
-# urlpatterns_descrip_suff = format_suffix_patterns(urlpatterns)
+urlpatterns_descrip_suff = format_suffix_patterns(urlpatterns)
--- app_descriptions/views.py
+++ app_descriptions/views.py
@@ -1,23 +1,16 @@
 from rest_framework import viewsets
 
-# from rest_framework.response import Response # not use
+from rest_framework.response import Response
 
 from app_descriptions.models import ProductDescription
 from app_descriptions.serializers import ProductDescriptionSerializer
-
-# from core.lang_utils import TranslateManager # not use
 
 
 class ProductDescriptionViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = ProductDescription.objects.all()
     serializer_class = ProductDescriptionSerializer
 
-    # not use
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     self.translate_manager = TranslateManager(self)
-
-    # def filter_by_prod(self, request, prod_pk, *args, **kwargs):
-    #     specif = ProductDescription.objects.filter(product_id=prod_pk)
-    #     serializer = self.get_serializer(specif, many=True)
-    #     return Response(serializer.data)
+    def filter_by_prod(self, request, prod_pk, *args, **kwargs):
+        specif = ProductDescription.objects.filter(id=prod_pk)
+        serializer = self.get_serializer(specif, many=True)
+        return Response(serializer.data)
