bfece48e7ab632cadc07b53d4b88e430d032c39a
RomanL91 <dev_roman91@mail.ru>
2025-01-02 18:26:51 +0500

    сущность ребер

--- app_sales_points/models.py
+++ app_sales_points/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 from core.mixins import JSONFieldsMixin
 
@@ -81,3 +83,73 @@ class Stock(models.Model):
     def __str__(self):
         # return f"{self.product.name_product} в {self.warehouse.name_warehouse} ({self.quantity} шт. по {self.price})"
         return f"{self.product.name_product} ({self.quantity} шт. по {self.price})"
+
+
+class Edges(models.Model):
+    edges_name = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name="Название направления",
+        help_text="Укажите имя для маршрута/направления/ребра. Дожно быть уникальным",
+    )
+    city_from = models.ForeignKey(
+        City,
+        on_delete=models.DO_NOTHING,
+        related_name="edges_from",
+        verbose_name="С города",
+        help_text="Укажите начальную точку маршрута",
+    )
+    city_to = models.ForeignKey(
+        City,
+        on_delete=models.DO_NOTHING,
+        related_name="edges_to",
+        verbose_name="В город",
+        help_text="Укажите конечную точку маршрута",
+    )
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+        limit_choices_to=models.Q(app_label="app_category", model="category")
+        | models.Q(app_label="app_brands", model="brands"),
+        verbose_name="Объект маршрута",
+        help_text="Выбирите к чему нужно применить маршрут",
+    )
+    object_id = models.PositiveIntegerField(editable=False)
+    obj = GenericForeignKey("content_type", "object_id")
+    estimated_delivery_days = models.PositiveIntegerField(
+        verbose_name="Срок доставки (в днях)",
+        default=3,
+        help_text="Количество дней для доставки по маршруту",
+    )
+    transportation_cost = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name="Транспортировочные расходы",
+        default=0.00,
+        help_text="Возможная наценка на объект маршрута",
+    )
+    is_active = models.BooleanField(
+        verbose_name="Активный статус",
+        default=True,
+        help_text="Определяет, является ли маршрут активным. Можно включить/выключить для использования",
+    )
+    expiration_date = models.DateField(
+        verbose_name="Дата истечения срока действия",
+        help_text="Дата, до которой (не включительно) маршрут используется системой",
+    )
+
+    class Meta:
+        verbose_name = "Маршрут"
+        verbose_name_plural = "Маршруты"
+        indexes = [
+            models.Index(fields=["content_type", "object_id"]),
+        ]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["city_from", "city_to", "object_id"],
+                name="unique_edge_constraint",  # Уникальное ограничение
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.city_from} -> {self.city_to}"
