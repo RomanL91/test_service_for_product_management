b47fa52d93704bad179d545123709618c2ea773e
RomanL91 <dev_roman91@mail.ru>
2025-01-02 18:52:25 +0500

    подключение маршрута для СЛИК + апи 2 версии

--- core/urls.py
+++ core/urls.py
@@ -12,23 +12,28 @@ from app_sales_points.urls import urlpatterns_stocks_suff
 from app_specifications.urls import urlpatterns_specif_suff
 from app_reviews.urls import urlpatterns_review_suff
 
-# from app_descriptions.urls import urlpatterns_descrip_suff # not use
+from app_descriptions.urls import urlpatterns_descrip_suff
 from app_brands.urls import urlpatterns_brands_suff
 from app_orders.urls import urlpatterns as url_orders_api
+
+# api v2
+from app_products.urls_v2 import urlpatterns as urlpatterns_products_v2
 
 urlpatterns = (
     [
         path("admin/", admin.site.urls),
         path("api/v1/", include(router.urls)),
+        path("api/v2/", include(urlpatterns_products_v2)),
         path("api/orders/", include(url_orders_api)),
         path("search/", include("app_elastic.urls")),
+        path("silk/", include("silk.urls", namespace="silk")),
     ]
     + urlpatterns_category_suff
     + urlpatterns_products_suff
     + urlpatterns_stocks_suff
     + urlpatterns_specif_suff
     + urlpatterns_review_suff
-    # + urlpatterns_descrip_suff # not use
+    + urlpatterns_descrip_suff
     + urlpatterns_brands_suff
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
