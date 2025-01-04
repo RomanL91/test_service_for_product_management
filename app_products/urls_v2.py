7f7bd82589de151c1c19fe64e06c9ddf3d68afa2
RomanL91 <dev_roman91@mail.ru>
2025-01-05 02:19:13 +0500

    ручное управление маршрутами, пока без кеширования

--- app_products/urls_v2.py
+++ app_products/urls_v2.py
@@ -1,10 +1,32 @@
-from rest_framework.routers import DefaultRouter
+from django.urls import path
+from django.views.decorators.cache import cache_page
 from app_products.views_v2 import ProductsViewSet_v2
 
-router = DefaultRouter()
-router.register(r"products_v2", ProductsViewSet_v2, basename="product_v2")
 
 urlpatterns = [
-    # Другие маршруты
-    *router.urls,
+    # path(
+    #     "products_v2/",
+    #     cache_page(60 * 15)(ProductsViewSet_v2.as_view({"get": "list"})),
+    #     name="products-list",
+    # ),
+    # path(
+    #     "products_v2/details/<slug:slug>/",
+    #     cache_page(60 * 15)(ProductsViewSet_v2.as_view({"get": "retrieve"})),
+    #     name="products-detail",
+    # ),
+    path(
+        "products_v2/",
+        ProductsViewSet_v2.as_view({"get": "list"}),
+        name="products-list",
+    ),
+    path(
+        "products_v2/details/<slug:slug>/",
+        ProductsViewSet_v2.as_view({"get": "retrieve"}),
+        name="products-detail",
+    ),
+    path(
+        "products_v2/filter_by_ids/",
+        ProductsViewSet_v2.as_view({"get": "filter_by_ids"}),
+        name="products-filter-by-ids",
+    ),
 ]
