4ac288565ee9b28e3732a0ac66880ff28cc3b572
RomanL91 <dev_roman91@mail.ru>
2025-01-06 23:43:07 +0500

    сериализатор для скидок + отзывов, логика их отдачи по приоритету

--- app_products/serializers_v2.py
+++ app_products/serializers_v2.py
@@ -6,6 +6,7 @@ from app_products.models import Products, ProductImage
 from app_sales_points.serializers import EdgeSerializer, StocksByCityField
 from app_brands.serializers import BrandSerializer
 from app_manager_tags.serializers import TagSerializer
+from app_reviews.serializers import ReviewsForProductsSerializer
 from app_specifications.serializers import SpecificationsSerializer
 from app_category.serializers_v2 import CategorySerializer
 
@@ -15,6 +16,13 @@ class ProductImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductImage
         fields = ["image"]  # Только поле изображения
+
+
+class DiscountShortSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+    amount = serializers.DecimalField(max_digits=10, decimal_places=2)
+    description = serializers.CharField()
 
 
 # Сериализатор для Products
@@ -32,12 +40,14 @@ class ProductSerializer(serializers.ModelSerializer):
     specifications = SpecificationsSerializer(
         many=True, read_only=True, source="prefetched_specifications"
     )  # Характеристики
+    reviews = serializers.SerializerMethodField()  # Ограничиваем отзывы через метод
     reviews_url = serializers.SerializerMethodField()  # Ссылка на отзывы к продукту
     desc_url = serializers.SerializerMethodField()  # Ссылка на описание к продукту
     related_products_url = (
         serializers.SerializerMethodField()
     )  # Ссылка на связанные продукты
     configuration_url = serializers.SerializerMethodField()  # Ссылка на конфигурации
+    discount = serializers.SerializerMethodField()
 
     class Meta:
         model = Products
@@ -54,13 +64,25 @@ class ProductSerializer(serializers.ModelSerializer):
             "avg_rating",
             "reviews_count",
             "stocks",
+            "discount",
             "tags",
             "specifications",
-            "desc_url",
+            "reviews",
             "reviews_url",
+            "desc_url",
             "related_products_url",
             "configuration_url",
         ]
+
+    def get_reviews(self, obj):
+        """
+        Возвращает только первые 20 отзывов.
+        """
+        if hasattr(obj, "reviews"):
+            # Ограничиваем первые 20 отзывов
+            limited_reviews = obj.reviews.all()[:20]
+            return ReviewsForProductsSerializer(limited_reviews, many=True).data
+        return []
 
     #
     # ========== Вспомогательный метод для формирования URL с ?ids=... ==========
@@ -114,3 +136,42 @@ class ProductSerializer(serializers.ModelSerializer):
         config_list = getattr(obj, "prefetched_configuration", [])
         request = self.context.get("request")
         return self._build_filter_by_ids_url(config_list, request)
+
+    def get_discount(self, obj):
+        """
+        1) Если есть скидка на товар - вернуть её (max)
+        2) Иначе смотреть скидку на категорию
+        3) Иначе скидку на бренд
+        """
+        # 1) Скидки на товар
+        product_discounts = getattr(obj, "prefetched_product_discounts", [])
+        if product_discounts:
+            # Вернуть  максимальную
+            disc = max(product_discounts, key=lambda d: d.amount)
+            return self._serialize_discount(disc)
+
+        # 2) Скидки на категорию
+        cat_discounts = []
+        if obj.category:
+            cat_discounts = getattr(obj.category, "prefetched_category_discounts", [])
+        if cat_discounts:
+            disc = max(cat_discounts, key=lambda d: d.amount)
+
+            return self._serialize_discount(disc)
+
+        # 3) Скидки на бренд
+        brand_discounts = []
+        if obj.brand:
+            brand_discounts = getattr(obj.brand, "prefetched_brand_discounts", [])
+        if brand_discounts:
+            disc = max(brand_discounts, key=lambda d: d.amount)
+
+            return self._serialize_discount(disc)
+
+        # Нет скидок
+        return None
+
+    def _serialize_discount(self, disc):
+        """Переводим объект скидки в удобный JSON."""
+        serializer = DiscountShortSerializer(disc)
+        return serializer.data
