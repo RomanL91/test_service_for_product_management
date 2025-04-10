413d21c5c4890004b362f79a58a2affdb7e54d0b
RomanL91 <dev_roman91@mail.ru>
2025-04-11 03:32:06 +0500

    очереди для загрузки характеристик

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,10 @@ python -m celery -A core.celery worker -l info -c 2 -P eventlet &
 python -m celery -A core beat -l info &
 python -m celery -A core flower --port=8001 --basic_auth=admin:admin &
 
-python manage.py search_index --rebuild -f
+# запуск скрипта для потребление очереди, где хранится результат парсинга
+python manage.py consumer_spec --host=185.100.67.246 --queue=returned_spec &
+
+# python manage.py search_index --rebuild -f
 
 # gunicorn --bind 0.0.0.0:8000 core.asgi -w 4 -k uvicorn.workers.UvicornWorker # с возможностью указания количества воркеров
 uvicorn core.asgi:application --host 0.0.0.0 --port 8888
