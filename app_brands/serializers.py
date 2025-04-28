211469d5cac0bd7b24d7bb385a03837f109c87ce
RomanL91 <dev_roman91@mail.ru>
2025-04-28 13:45:11 +0500

    сериализатор для вывода лого бренда

--- app_brands/serializers.py
+++ app_brands/serializers.py
@@ -1,13 +1,26 @@
 from rest_framework import serializers
 
-from app_brands.models import Brands
+from app_brands.models import Brands, LogoBrand
+
+
+class LogoBrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LogoBrand
+        fields = ["image"]
 
 
 class BrandSerializer(serializers.ModelSerializer):
+    logo = LogoBrandSerializer(
+        many=True,
+        read_only=True,
+        source="logobrand_set",
+    )
+
     class Meta:
         model = Brands
         fields = [
             "id",
             "name_brand",
             "additional_data",
+            "logo",
         ]
